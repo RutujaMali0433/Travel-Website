6ae7f4764f63ef80837f1d7eeeab49b122529fc7
Rutuja Mali <120778805+RutujaMali0433@users.noreply.github.com>
2023-04-18 21:14:13 +0530

    commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,62 @@
+import "./FooterStyles.css";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <div className="top">
+                <div>
+                    <h1>Tour&Travels</h1>
+                    <p>Choose your favourite destination.</p>
+                </div>
+                <a href="/">
+                    <i className="fa-brands fa-facebook-square"></i>
+                </a>
+                <a href="/">
+                    <i className="fa-brands fa-instagram-square"></i>
+                </a>
+                <a href="/">
+                    <i className="fa-brands fa-twitter-square"></i>
+                </a>
+                <a href="/">
+                    <i className="fa-brands fa-behnace-square"></i>
+                </a>
+            </div>
+
+
+            <div className="bottom">
+                <div>
+                    <h4>Project</h4>
+                    <a href="/">Changelog</a>
+                    <a href="/">Status</a>
+                    <a href="/">License</a>
+                    <a href="/">All Versions</a>
+                </div>
+
+                <div>
+                    <h4>Community</h4>
+                    <a href="/">GitHub</a>
+                    <a href="/">Issues</a>
+                    <a href="/">Project</a>
+                    <a href="/">Twitter</a>
+                </div>
+
+                <div>
+                    <h4>Help</h4>
+                    <a href="/">Support</a>
+                    <a href="/">Troubleshooting</a>
+                    <a href="/">Contact us</a>
+                    <a href="/">All Versions</a>
+                </div>
+
+                <div>
+                    <h4>Others</h4>
+                    <a href="/">Terms of Service</a>
+                    <a href="/">Privacy Policy</a>
+                    <a href="/">License</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
